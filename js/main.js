10b45672f6435272a89e6f0269153643ce9d245b
vargak84 <einstein.xxx@gmail.com>
2020-06-22 11:42:13 +0200

    new

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 function calcAmount() {
     let price = 1000;
     let amountInput = document.querySelector("input[name='amount-input']");
-    let amount = parseInt(amountInput.value) * price;
-    alert("Fizetendő: " + amount + "Ft");
+    let showAmount = document.querySelector("span.showAmount");
+    let amountNumber = parseInt(amountInput.value);
+    amountNumber = isNaN(amountNumber) ? 0 : amountNumber
+    
+    if ( amountNumber > 10 ) {
+        alert ("Maximum 10db vásárolható");
+    } else if ( amountNumber < 1 ) {
+        alert ("Minimum 1db-ot kell vásárolni");
+    } else {
+        let amount = amountNumber * price;
+
+    showAmount.innerHTML = amount;
+    }
+    
 }
